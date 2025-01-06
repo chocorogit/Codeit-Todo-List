e99daefa579894ae74fa2f312516a1ef842638bb
Jiyeon Han <chocorogit@gmail.com>
2025-01-06 23:06:17 +0900

    style : 레이아웃 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import Header from './components/Header';
+import TodoList from './components/TodoList';
+import DoneList from './components/DoneList';
+import Form from './components/Form';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <div className={'w-full h-[60px] border border-b-slate-200'}>
+        {/* 헤더 */}
+        <Header />
+        {/* 컨텐츠 */}
+        <main
+          className={
+            'max-w-7xl w-full px-4 tablet:px-6 mx-auto leading-[1.125]'
+          }
+        >
+          <Form />
+          {/* 할 일 리스트 영역 */}
+          <TodoList />
+          {/* 완료 리스트 영역 */}
+          <DoneList />
+        </main>
+      </div>
+    </>
+  );
 }
