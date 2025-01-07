a22a609811f897d6bc072557c01f8ee7d81b59e6
Jiyeon Han <chocorogit@gmail.com>
2025-01-07 18:42:37 +0900

    chore : 헤더 구조 수정

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,29 +3,31 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header
-      className={
-        'flex align-middle items-center max-w-7xl w-full h-full laptop:px-10 tablet:px-6 px-4 mx-auto'
-      }
-    >
-      <h1 className={'cursor-pointer'}>
-        <Link href='/'>
-          <Image
-            className={'tablet:hidden'}
-            src='/images/mobile-logo.svg'
-            width='71'
-            height='40'
-            alt='doit-mobile-logo'
-          />
-          <Image
-            className={'hidden tablet:block'}
-            src='/images/logo.svg'
-            width='151'
-            height='40'
-            alt='doit-logo'
-          />
-        </Link>
-      </h1>
-    </header>
+    <div className={'w-full h-[60px] border-b border-b-slate-200 bg-white'}>
+      <header
+        className={
+          'flex align-middle items-center max-w-7xl w-full h-full laptop:px-10 tablet:px-6 px-4 mx-auto'
+        }
+      >
+        <h1 className={'cursor-pointer'}>
+          <Link href='/'>
+            <Image
+              className={'tablet:hidden'}
+              src='/images/mobile-logo.svg'
+              width='71'
+              height='40'
+              alt='doit-mobile-logo'
+            />
+            <Image
+              className={'hidden tablet:block'}
+              src='/images/logo.svg'
+              width='151'
+              height='40'
+              alt='doit-logo'
+            />
+          </Link>
+        </h1>
+      </header>
+    </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { TodoProvider } from './context/TodoContext';
 import Header from './components/Header';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Todo List',
@@ -26,24 +15,18 @@ export default function RootLayout({
 }>) {
   return (
     <TodoProvider>
-      <html lang='en'>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased  bg-gray-50`}
-        >
-          <div
-            className={'w-full h-[60px] border-b border-b-slate-200 bg-white'}
+      <html lang='ko'>
+        <body className={'bg-gray-50'}>
+          {/* 헤더 */}
+          <Header />
+          {/* 컨텐츠 */}
+          <main
+            className={
+              'max-w-7xl w-full px-4 laptop:px-10 tablet:px-6 mx-auto leading-[1.125] '
+            }
           >
-            {/* 헤더 */}
-            <Header />
-            {/* 컨텐츠 */}
-            <main
-              className={
-                'max-w-7xl w-full px-4 laptop:px-10 tablet:px-6 mx-auto leading-[1.125] tablet:pt-6 pt-4'
-              }
-            >
-              {children}
-            </main>
-          </div>
+            {children}
+          </main>
         </body>
       </html>
     </TodoProvider>
